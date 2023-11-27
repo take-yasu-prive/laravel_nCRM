6b024ef91f090a3433752a65c488cb0757661459
take-yasu-prive <take-yasu.0315@au.com>
2023-11-27 23:50:55 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,19 @@
+<script setup>
+import GuestLayout from '@/Layouts/Guest.vue'
+import Label from '@/Components/Label.vue'
+import Input from '@/Components/Input.vue'
+
+const emitTest = e => console.log(e)
+
+</script>
+
+<template>
+  <GuestLayout>
+    <Label value="件名">タイトル</Label>
+    <Input 
+      modelValue="初期値が入ります"
+      @update:modelValue="emitTest"
+    ></input>
+
+  </GuestLayout>
+</template>
