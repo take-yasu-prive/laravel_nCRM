1577c3bf1de0df89cbe5f5f8781c9bfea740603a
take-yasu-prive <take-yasu.0315@au.com>
2023-12-22 22:44:59 +0900

    decile

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -5,6 +5,7 @@ import { reactive, onMounted } from "vue";
 import { getToday } from "@/common";
 import axios from "axios";
 import Chart from "@/Components/Chart.vue";
+import ResultTable from "@/Components/ResultTable.vue";
 
 onMounted(() => {
   form.startDate = getToday();
@@ -33,6 +34,7 @@ const getData = async () => {
         data.data = res.data.data;
         data.labels = res.data.labels;
         data.totals = res.data.totals;
+        data.type = res.data.type;
         console.log(res.data);
       });
   } catch (e) {
@@ -54,6 +56,26 @@ const getData = async () => {
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
           <div class="p-6 bg-white border-b border-gray-200">
             <form @submit.prevent="getData">
+              分析方法<br />
+              <input
+                type="radio"
+                v-model="form.type"
+                value="perDay"
+                checked
+              /><span class="mr-2">日別</span>
+              <input type="radio" v-model="form.type" value="perMonth" /><span
+                class="mr-2"
+                >月別</span
+              >
+              <input type="radio" v-model="form.type" value="perYear" /><span
+                class="mr-2"
+                >年別</span
+              >
+              <input type="radio" v-model="form.type" value="decile" /><span
+                class="mr-2"
+                >デシル分析</span
+              ><br />
+
               From：<input
                 type="date"
                 name="startDate"
@@ -72,37 +94,7 @@ const getData = async () => {
             </form>
             <div v-show="data.data">
               <Chart :data="data" />
-            </div>
-            <div
-              v-show="data.data"
-              class="lg:w-2/3 w-full mx-auto overflow-auto"
-            >
-              <table class="table-auto w-full text-left whitespace-no-wrap">
-                <thead>
-                  <tr>
-                    <th
-                      class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl"
-                    >
-                      年月日
-                    </th>
-                    <th
-                      class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"
-                    >
-                      金額
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="item in data.data" :key="item.date">
-                    <td class="border-b-2 border-grey-200 px-4 py-3">
-                      {{ item.date }}
-                    </td>
-                    <td class="border-b-2 border-grey-200 px-4 py-3">
-                      {{ item.total }}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+              <ResultTable :data="data" />
             </div>
           </div>
         </div>
